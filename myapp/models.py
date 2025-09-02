fb64f7a0c1e0a783ba9abd7ccfa60d09e1e84b94
Arthur A. <arthurakilalves@gmail.com>
2025-09-02 15:12:05 -0300

    feat: criando novos models

--- myapp/models.py
+++ myapp/models.py
@@ -8,3 +8,17 @@ class Client(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+        
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    client = models.ForeignKey(Client,on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date_order = models.DateTimeField(auto_now_add=True)
+
