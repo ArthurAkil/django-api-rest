7a47bccf8e2eb02c181e0abdf2dd3f6cc2ab2156
Arthur A. <arthurakilalves@gmail.com>
2025-09-03 22:58:47 -0300

    feat: adicionando no admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Client
+from .models import Client, Product, Order
 
 
 @admin.register(Client)
@@ -8,3 +8,13 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ("name", "email", "birth_date",)
     search_fields = ("name", "email", "birth_date",)
     list_filter = ("name", "email", "birth_date",)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "stock")
+    search_fields = ("name", "price", "stock")
+    list_filter = ("name", "price", "stock")
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("client","date_order",)
